a72f1c49708dd1b80ec89755b477692f7e3241fe
Mayuri Gaikwad <GS-1981@GSLAB.COM>
2018-10-08 16:10:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+//var addition =require('./ModuleDemo.js');
+//console.log(addition.AddNumber(1,2));
+
+var tutor=require('./NodeTutorial.js');
+//tutor.NodeTutorial();
+//tutor.nodetutorial().pt();
+var obj= new tutor.NodeTutorial();
+obj.pTutor();
